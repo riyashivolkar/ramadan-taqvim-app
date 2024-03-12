c96690395a074c22947040a40acbba68519a9b8f
unknown <riyashivolkar2@gmail.com>
2024-03-12 18:55:18 +0530

    added checkbox for observed fasting days

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,12 +1,17 @@
-import {useCallback, useState} from "react";
-import {filterRegions} from "@/constants/filterRegions";
+import { useCallback, useState } from "react";
+import { filterRegions } from "@/constants/filterRegions";
 
-export default function useFilter () {
-    const [filter, setFilter] = useState(null);
+export default function useFilter() {
+  const [filter, setFilter] = useState(null);
 
-    const onFilterByRegionSelected = useCallback((e) => {
-        setFilter(filterRegions.find((region) => region.id === Number(e.target?.value)))
-    }, [filterRegions])
+  const onFilterByRegionSelected = useCallback(
+    (e) => {
+      setFilter(
+        filterRegions.find((region) => region.id === Number(e.target?.value))
+      );
+    },
+    [filterRegions]
+  );
 
-    return {filter, onFilterByRegionSelected, filterRegions}
+  return { filter, onFilterByRegionSelected, filterRegions };
 }
